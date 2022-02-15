d5293d562ba64340bc1f3e35982cd3fb33108987
Awadabang <1765256254@qq.com>
2022-02-15 15:07:37 +0800

    init

---
+++ src/pages/Help.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel saepe nam
+      ipsa modi deleniti, eum praesentium quod sit incidunt consequuntur fugiat
+      velit architecto impedit eos quaerat expedita odit similique? Enim?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione dicta
+      quos dignissimos aliquam perferendis a magnam dolorum itaque. Voluptatum,
+      atque culpa veritatis quod obcaecati sed saepe nesciunt. Atque, pariatur.
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
